9ede4776f01169a26b4a29c6a4aac5f0c5d9f21b
Craig Shanks <44571906+crshanks@users.noreply.github.com>
2025-10-10 17:35:23 +0100

    feat: implement proportional cost allocation with synthetics pricing - Add proportional allocation logic for minimum commitments - Implement synthetics cost allocation beyond included checks

--- dashboard.tf
+++ dashboard.tf
@@ -11,28 +11,30 @@ locals {
   ]))
 
   templatefile_render = templatefile(
-   "${path.module}/dashboards/dashboard.json.tftpl",
+    "${path.module}/dashboards/dashboard.json.tftpl",
     {
-      tf_showback_config = var.showback_config
-      tf_dashboard_name = var.dashboard_name
-      tf_event_name_prefix = var.event_name_prefix
-      tf_metric_name_prefix = var.metric_name_prefix
-      tf_account_id = var.showback_insert_account_id
-      tf_core_user_usd = var.showback_price.core_user_usd
-      tf_full_user_usd = var.showback_price.full_user_usd
-      tf_gb_ingest_usd = var.showback_price.gb_ingest_usd
-      tf_core_user_committed_usd = var.showback_price.core_user_committed_usd
-      tf_full_user_committed_usd = var.showback_price.full_user_committed_usd
-      tf_gb_ingest_committed_usd = var.showback_price.gb_ingest_committed_usd
-      tf_core_user_additional_usd = var.showback_price.core_user_additional_usd
-      tf_full_user_additional_usd = var.showback_price.full_user_additional_usd
-      tf_gb_ingest_additional_usd = var.showback_price.gb_ingest_additional_usd
-      tf_min_core_users = var.showback_price.min_core_users
-      tf_min_full_users = var.showback_price.min_full_users
-      tf_min_gb_ingest = var.showback_price.min_gb_ingest
-      tf_prorate_minimums = var.showback_price.prorate_minimums
-      tf_unique_tiers = local.unique_tiers
-      tf_unique_accounts = local.unique_accounts
+      tf_showback_config                 = var.showback_config
+      tf_dashboard_name                  = var.dashboard_name
+      tf_event_name_prefix               = var.event_name_prefix
+      tf_metric_name_prefix              = var.metric_name_prefix
+      tf_account_id                      = var.showback_insert_account_id
+      tf_core_user_usd                   = var.showback_price.core_user_usd
+      tf_full_user_usd                   = var.showback_price.full_user_usd
+      tf_gb_ingest_usd                   = var.showback_price.gb_ingest_usd
+      tf_core_user_committed_usd         = var.showback_price.core_user_committed_usd
+      tf_full_user_committed_usd         = var.showback_price.full_user_committed_usd
+      tf_gb_ingest_committed_usd         = var.showback_price.gb_ingest_committed_usd
+      tf_core_user_additional_usd        = var.showback_price.core_user_additional_usd
+      tf_full_user_additional_usd        = var.showback_price.full_user_additional_usd
+      tf_gb_ingest_additional_usd        = var.showback_price.gb_ingest_additional_usd
+      tf_min_core_users                  = var.showback_price.min_core_users
+      tf_min_full_users                  = var.showback_price.min_full_users
+      tf_min_gb_ingest                   = var.showback_price.min_gb_ingest
+      tf_synthetics_checks_included      = var.showback_price.synthetics_checks_included
+      tf_synthetics_additional_check_usd = var.showback_price.synthetics_additional_check_usd
+      tf_prorate_minimums                = var.showback_price.prorate_minimums
+      tf_unique_tiers                    = local.unique_tiers
+      tf_unique_accounts                 = local.unique_accounts
     }
   )
 }
@@ -45,13 +47,13 @@ resource "newrelic_one_dashboard_json" "templatefile_dashboard" {
 resource "newrelic_entity_tags" "templatefile_dashboard" {
   guid = newrelic_one_dashboard_json.templatefile_dashboard.guid
   tag {
-    key = "terraform"
+    key    = "terraform"
     values = [true]
   }
 }
 
 output "templatefile_dashboard" {
-  value=newrelic_one_dashboard_json.templatefile_dashboard.permalink 
+  value = newrelic_one_dashboard_json.templatefile_dashboard.permalink
 }
 
 output "unique_tiers" {
